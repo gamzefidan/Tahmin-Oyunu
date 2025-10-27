984e5887e28513377fedbcb0a692f3dc650f7215
Gamze Fidan <gamzefidan99@gmail.com>
2025-10-27 17:57:40 +0300

    Tahmin Oyunu

---
+++ src/TahminOyunu.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import './TahminOyunu.css';
+
+export function TahminOyunu() {
+
+    const[rastgeleSayi,setRastgeleSayi]= useState(Math.floor(Math.random()*100)+1);
+    const[tahmin,setTahmin]= useState('');
+    const[mesaj,setMesaj]=useState("");
+    const[denemeSayisi,setDenemeSayisi]= useState(0);
+    
+
+    const tahminKontrolEt = () => {
+        const sayi = parseInt(tahmin);
+        setDenemeSayisi(denemeSayisi +1);
+        if(sayi===rastgeleSayi){
+            setMesaj(`Tebrikler! ${denemeSayisi+1} denemede doğru tahmin ettiniz.`);
+        } else if (sayi < rastgeleSayi){
+            setMesaj("Daha yüksek bir sayı deneyin.");
+        } else if (sayi > rastgeleSayi){
+            setMesaj("Daha düşük bir sayı deneyin.");
+        }
+    };
+    const oyunuSifirla= () => {
+        setRastgeleSayi(Math.floor(Math.random()*100)+1);
+        setTahmin('');
+        setMesaj('');
+        setDenemeSayisi(0);
+    };
+
+    return(
+        <div className="oyun kapsayici">
+        
+        <h2> 1 ile 100 arasında bir sayı tahmin edin </h2>
+
+        <input
+        type="number"
+        value={tahmin}
+        onChange={(e) => setTahmin (e.target.value)}
+        placeholder="tahmininizi girin"
+        className="oyun-input"
+       
+        />
+        <button onClick={tahminKontrolEt} className="oyun-buton tahmin-buton">
+            Tahmin Et
+            </button>
+
+        <button onClick={oyunuSifirla} className="oyun-buton sifirla-buton">
+            Oyunu Sıfırla
+            
+            </button>
+         <p className={`oyun-mesaj ${mesaj.includes("Tebrikler") ? "kazandi" : ""}`}>
+        {mesaj}
+      </p>
+
+      {mesaj.includes("Tebrikler") && (
+        <button onClick={oyunuSifirla} className="oyun-buton yeni-oyun-buton">
+          Yeni Oyun Başlat
+        </button>
+      )}
+        </div>
+
+       );
+}
